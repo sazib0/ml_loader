a7cb84ed954d7602c671cf5b02767007eee7547b
Sazib <sazibc5@gmail.com>
2024-11-01 12:47:58 +0600

    updated load_data package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_loader',
+    name='ml_loader',
     version='0.1',
     packages=find_packages(),
     install_requires=[
@@ -13,4 +13,13 @@ setup(
         'lxml'
     ],
     description='A package to load data files based on their file extension.',
+    author='Sazib',
+    author_email='sazibc5@gmail.com',
+    url='https://github.com/sazib0/ml_loader',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD 3-Clause License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
